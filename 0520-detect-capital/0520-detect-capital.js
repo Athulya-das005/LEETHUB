57ef144ad2e00de2dda3199e2539073a618b808b
Athulya-das005 <athulyadas005@gmail.com>
2025-01-30 09:43:47 +0530

    Time: 0 ms (100%), Space: 50.7 MB (42.2%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+
+ 
+var detectCapitalUse = function(word) {
+    return word === word.toUpperCase() || word === word[0] + word.substr(1).toLowerCase();
+};
