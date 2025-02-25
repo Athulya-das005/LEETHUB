6eca88f21e02b86a264142581b694a07c15b2012
Athulya-das005 <athulyadas005@gmail.com>
2025-02-25 10:29:04 +0530

    Time: 2 ms (29.08%), Space: 57.6 MB (5.3%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    for (let i = 0; i < 31; i++) {
+            let ans = Math.pow(2, i);
+            if (ans === n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
