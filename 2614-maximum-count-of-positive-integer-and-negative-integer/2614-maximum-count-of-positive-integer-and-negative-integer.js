42e043d58749112d277d3dbe35f0a8ddcd04f259
Athulya-das005 <athulyadas005@gmail.com>
2025-03-12 09:53:54 +0530

    Time: 0 ms (100%), Space: 56.2 MB (9.2%) - LeetHub

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,8 @@
+const maximumCount = (nums) => {
+    let positives = 0, negatives = 0;
+    for (let value of nums) {
+        positives += value > 0;
+        negatives += value < 0;
+    }
+    return Math.max(positives, negatives);
+};
