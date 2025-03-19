980c11098af9c9806b56e8687a3c1e0831435077
Athulya-das005 <athulyadas005@gmail.com>
2025-03-19 09:52:45 +0530

    Time: 46 ms (85%), Space: 55.9 MB (42.61%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,19 @@
+function createCounter(init) {
+  let current = init; 
+  
+  return {
+ increment: function() {
+ current++;
+return current;
+    },
+decrement: function() {
+    current--;
+return current;
+    },
+reset: function() {
+current = init;
+return current;
+    }
+  };
+}
+
